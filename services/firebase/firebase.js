4e42deb2ad3de5b0faba99d24a2465fa6a525c1a
Oderinwale Oluwafemi Peter <oderinwalefemi150@gmail.com>
2021-11-25 10:39:23 +0100

    security fix

--- services/firebase/firebase.js
+++ services/firebase/firebase.js
@@ -1,6 +1,6 @@
 const firebase = require("firebase-admin");
 
-let serviceAccount = require("./game-test-f3300-firebase-adminsdk-5mn6o-4302695558.json");
+let serviceAccount = require("../../game-test-f3300-firebase-adminsdk-5mn6o-4302695558.json");
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
   databaseURL: "https://game-test-f3300.firebaseio.com",
